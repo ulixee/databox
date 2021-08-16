b45b72a0c54a640728622eca7dcfaba49e850736
blakebyrnes <blakebyrnes@gmail.com>
2021-08-15 21:49:34 -0400

    fix(client): add datatyping to hero client

--- client/index.ts
+++ client/index.ts
@@ -1,8 +1,16 @@
 import IConnectionToCoreOptions from '@ulixee/databox-interfaces/IConnectionToCoreOptions';
+import type { IHeroCreateOptions } from '@ulixee/hero';
 import { Observable } from './lib/ObjectObserver';
 import PackagedDatabox from './lib/PackagedDatabox';
 import ConnectionToCore from './connections/ConnectionToCore';
+import RunningDatabox from './lib/RunningDatabox';
 
 export { Observable, IConnectionToCoreOptions, ConnectionToCore };
+
+declare module '@ulixee/hero' {
+  interface IHeroCreateOptions {
+    databox?: RunningDatabox;
+  }
+}
 
 export default PackagedDatabox;
