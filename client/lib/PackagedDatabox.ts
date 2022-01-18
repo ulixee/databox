b459950ba9f2cd36dccdc078508710fbc573806d
Blake Byrnes <blakebyrnes@gmail.com>
2022-01-17 22:01:41 -0500

    feat: remove hero from databox

--- client/lib/PackagedDatabox.ts
+++ client/lib/PackagedDatabox.ts
@@ -1,6 +1,5 @@
 import IDataboxRunOptions from '@ulixee/databox-interfaces/IDataboxRunOptions';
 import IPackagedDatabox from '@ulixee/databox-interfaces/IPackagedDatabox';
-import './utils/heroHook';
 import readCommandLineArgs from './utils/readCommandLineArgs';
 import RunningDatabox from './RunningDatabox';
 import IComponents, { IScriptFn } from '../interfaces/IComponents';
@@ -36,8 +35,12 @@ export default class PackagedDatabox implements IPackagedDatabox {
   public async run(options: IDataboxRunOptions = {}): Promise<void> {
     const { createRunningDatabox } = this.constructor as typeof PackagedDatabox;
     try {
-      this.runningDatabox = await createRunningDatabox.call(this, options);
-      await this.#components.scriptFn(this.runningDatabox);
+      this.runningDatabox = await createRunningDatabox.call(
+        this,
+        options,
+        this.createRunningDataboxFn.bind(this),
+      );
+      await this.runScript();
       return this.runningDatabox.output;
     } catch (error) {
       this.runningDatabox?.emit('error', error);
@@ -48,18 +51,30 @@ export default class PackagedDatabox implements IPackagedDatabox {
     }
   }
 
-  public static createRunningDatabox(options: IDataboxRunOptions = {}): Promise<RunningDatabox> {
-    return new Promise<RunningDatabox>(async (resolve, reject) => {
-      try {
-        const createConnectionToCoreFn = this.createConnectionToCoreFn;
-        const connectionManager = new ConnectionManager({ createConnectionToCoreFn, ...options });
-        await connectionManager.getConnectedCoreSessionOrReject();
-        process.nextTick(() => {
-          resolve(new RunningDatabox(connectionManager, options));
-        });
-      } catch (error) {
-        reject(error);
-      }
-    });
+  protected async runScript(): Promise<void> {
+    await this.#components.scriptFn(this.runningDatabox);
+  }
+
+  protected createRunningDataboxFn(
+    connectionManager: ConnectionManager,
+    options: IDataboxRunOptions,
+  ): Promise<RunningDatabox> {
+    return Promise.resolve(new RunningDatabox(connectionManager, options));
+  }
+
+  public static async createRunningDatabox(
+    options: IDataboxRunOptions = {},
+    createRunningDataboxFn?: (
+      connectionManager: ConnectionManager,
+      options: IDataboxRunOptions,
+    ) => Promise<RunningDatabox>,
+  ): Promise<RunningDatabox> {
+    const createConnectionToCoreFn = this.createConnectionToCoreFn;
+    const connectionManager = new ConnectionManager({ createConnectionToCoreFn, ...options });
+    await connectionManager.getConnectedCoreSessionOrReject();
+    await new Promise(process.nextTick);
+    return createRunningDataboxFn
+      ? await createRunningDataboxFn(connectionManager, options)
+      : new RunningDatabox(connectionManager, options);
   }
 }
